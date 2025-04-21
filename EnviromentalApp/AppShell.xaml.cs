607a2e5da27fddefdf1c534adb9226d04ad4150b
Callum Glass <40589677@live.napier.ac.uk>
2025-04-21 16:14:11 +0100

    Initial Commit

---
+++ EnviromentalApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EnviromentalApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ EnviromentalApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace EnviromentalApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
